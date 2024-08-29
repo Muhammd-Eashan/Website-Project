2385a8bbaf65fa24670d6e26c0ccb8e1c3de72c0
Muhammd-Eashan <muhammadeashan66@gmail.com>
2024-08-29 20:59:05 +0500

    completed the header with its java script work too

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,1 +1,73 @@
+// ALL CATEGORIES BUTTON
 
+const stickyNav = document.querySelector('.sticky-nav');
+const closeSide = document.querySelector('.close-side');
+const allCategoriesBtn = document.getElementById('all-categories-btn');
+
+function collapseSidebar() {
+    stickyNav.classList.remove('expanded');
+    closeSide.classList.remove('visible');
+}
+
+allCategoriesBtn.addEventListener('click', function() {
+    stickyNav.classList.toggle('expanded');
+    closeSide.classList.toggle('visible');
+});
+
+closeSide.addEventListener('click', function(event) {
+    collapseSidebar();
+});
+
+stickyNav.addEventListener('mouseleave', function() {
+    collapseSidebar();
+});
+
+// SHOPPING CART BUTTON
+
+function collapseShoppingbar(){
+    document.querySelector(".shopping-cart").classList.remove("opened")
+    closeSide.classList.remove('seen');
+}
+
+document.getElementById("shopping-cart-btn").addEventListener("click", function(){
+    document.querySelector(".shopping-cart").classList.toggle("opened")
+    closeSide.classList.toggle('seen');
+});
+closeSide.addEventListener('click', function(event) {
+    collapseShoppingbar()
+});
+document.getElementById("close-btn").addEventListener('click', function(event) {
+    collapseShoppingbar()
+});
+
+// SIGN IN BUTTON
+
+function collapseSignbar(){
+    document.querySelector(".sign-in-form").classList.remove("opened")
+    closeSide.classList.remove('seen');
+}
+
+document.getElementById("sign-in-btn").addEventListener("click", function(){
+    document.querySelector(".sign-in-form").classList.toggle("opened")
+    closeSide.classList.toggle('seen');
+});
+closeSide.addEventListener('click', function(event) {
+    collapseSignbar()
+});
+document.getElementById("close-btn-2").addEventListener('click', function(event) {
+    collapseSignbar()
+});
+
+// SHOW PASSWORD BUTTON
+const Input = document.getElementById("input")
+const Eye = document.getElementById("eye")
+document.getElementById("show-password-btn").addEventListener("click", function(){
+    let type = Input.getAttribute("type")
+        if (type == "password") {
+            Input.setAttribute("type","text")
+            Eye.classList.add("slash")
+        } else {
+            Input.setAttribute("type","password")
+            Eye.classList.remove("slash")
+        }
+});

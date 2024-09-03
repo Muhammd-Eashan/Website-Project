e1a7d0cceac26b9c80c59cf2b3b3949cf96c9c3a
Muhammd-Eashan <muhammadeashan66@gmail.com>
2024-09-03 19:40:16 +0500

    First row of the home page is completed

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -71,3 +71,27 @@ document.getElementById("show-password-btn").addEventListener("click", function(
             Eye.classList.remove("slash")
         }
 });
+
+// COUNTDOWN TIMER
+
+// Set the target date and time
+const targetDate = Date.UTC(2025, 0, 1, 0, 0, 0,);
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const timeLeft = targetDate - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerText = days;
+  document.getElementById('hours').innerText = hours;
+  document.getElementById('minutes').innerText = minutes;
+  document.getElementById('seconds').innerText = seconds;
+}
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
+

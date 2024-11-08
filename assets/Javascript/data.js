e527cd1d8a36e600a51d8e0f2c5ea0eac7a2c10b
Muhammad-Eashan <muhammadeashan66@gmail.com>
2024-11-08 08:19:16 +0500

    fourth row completed

--- assets/Javascript/data.js
+++ assets/Javascript/data.js
@@ -2,7 +2,6 @@ let products = [
     {
        "id" : 1,
        "name" : "Apple MacBook Pro 16″ M1 Pro",
-       "category" : "Apple MacBook",
        "discountedprice" : 2499,
        "sku" : 30876,
        "img" : "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/10/apple-macbook-pro-16-silver-1.jpg",
@@ -10,25 +9,20 @@ let products = [
     {
         "id" : 2,
         "name" : "Oculus Quest 2",
-        "category" : "VR Headsets",
         "discountedprice" : 449,
-        "originalprice" : 499,
         "sku" : 608069,
         "img" : "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/11/oculus-quest-2-1.jpg",
        },
     {
         "id" : 3,
         "name" : "Asus GeForce GTX 1660 Ti TUF",
-        "category" : "Graphics Cards",
         "discountedprice" : 269,
-        "originalprice" : 315,
         "sku" : 5011633,
         "img" : "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/11/asus-geforce-gtx-1660-ti-tuf-1.jpg",
     },
     {
         "id" : 4,
         "name" : "Samsung Neo QLED 55QN85A",
-        "category" : "OLED TV",
         "discountedprice" : 1600,
         "sku" : 631046,
         "img" : "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/11/samsung-neo-qled-55qn85a-1.jpg",
@@ -36,11 +30,37 @@ let products = [
     {
         "id" : 5,
         "name" : "LG FH4G1BCS2",
-        "category" : "Washing Machines",
         "discountedprice" : 945,
-        "originalprice" : 1110,
         "sku" : 730053,
         "img" : "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/11/lg-fh4g1bcs2-1.jpg",
+    },
+    {
+        "id" : 6,
+        "name" : "Acer ConceptD 7 Ezel",
+        "discountedprice" : 945,
+        "sku" : 730053,
+        "img" : "https://woodmart.b-cdn.net/mega-electronics/wp-content/uploads/sites/9/2022/10/acer-conceptd-7-ezel-1.jpg",
+    },
+    {
+        "id" : 7,
+        "name" : "ACER ConceptD CT300",
+        "discountedprice" : 1199,
+        "sku" : 30944,
+        "img" : "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/10/acer-conceptd-ct300-1.jpg",
+    },
+    {
+        "id" : 8,
+        "name" : "Acer Predator Helios 300",
+        "discountedprice" : 1600,
+        "sku" : 30894,
+        "img" : "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/10/acer-predator-helios-300-1.jpg",
+    },
+    {
+        "id" : 9,
+        "name" : "Acer ProDesigner PE320QK",
+        "discountedprice" : 750,
+        "sku" : 2144207,
+        "img" : "https://woodmart.b-cdn.net/mega-electronics/wp-content/uploads/sites/9/2022/10/acer-prodesigner-pe320qk-1.jpg",
     },
 ]
 

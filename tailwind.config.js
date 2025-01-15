7a16258097c1a47abb27220e175ac368555f49d9
Muhammad-Eashan <muhammadeashan66@gmail.com>
2025-01-16 04:53:21 +0500

    project completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',           // Include your HTML files
+    './assets/Style/style.css',
+    './assets/Javascript/javascript.js', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
